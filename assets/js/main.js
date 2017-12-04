ebb55b9756de60d32cfc4b3003f9af563be8177c
Robo Wonder <robowonder@outlook.com>
2017-12-04 22:24:34 +0700

    fix js

--- assets/js/main.js
+++ assets/js/main.js
@@ -103,7 +103,7 @@ $(function() {
     if (filter === 'recent') {
       toc.slice(0, {{ site.recent_num }}).fadeIn(350);
     } else {
-      $('.toc-link[data-tags~=' + filter + ']').fadeIn(350);
+      $('.toc-link[data-tags="' + filter + '"]').fadeIn(350);
     }
     $(this).addClass('active').siblings().removeClass('active');
   });
